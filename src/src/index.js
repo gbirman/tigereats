e64a1d0f6e482420470cfdb357a134a48c07fcb7
Gabriel <25272206+gbirman@users.noreply.github.com>
2020-08-22 17:41:10 -0400

    removed src folder

--- src/src/index.js
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppRouter from './routers/AppRouter';
-
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
